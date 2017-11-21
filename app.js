d27eefb451ac336aa6168bf94384ca12033d5a37
John Wells <molekewl2017@gmail.com>
2017-11-21 14:59:32 -0800

    build first component & child component for productList

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-class ProductList extends React.Component {
-	constructor() {
-		super();
-	}
+// import React, { Component } from 'react';
 
+// 1st Component ProductList
+// renders all list of products w/ its sub-components / child components
+class ProductList extends React.Component {  
   render() {
     return (
       <div className='ui items'>
-        Hello, Friend! I am a basic react component.
+        <Product />
       </div>
     );
-	}
+  }
 }
 
+// child component - contains the product listing
+// every product we'll add an image, a title, description & an avatar of the post author
+const Product = React.createClass({
+  render() {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src='images/products/image-aqua.png' />
+        </div>
+        <div className='midle aligned content'>
+          <div className='description'>
+            <a>Fort Knight</a>
+            <p>Authentic renaissnce actors, delivered in just two weeks</p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src='images/avatar/daniel.jpg'
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-	<ProductList />,
-	document.getElementById('content')
+  <ProductList />,
+  document.getElementById('content')
 );
